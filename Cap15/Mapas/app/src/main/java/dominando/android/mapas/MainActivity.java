354943197b0a55c0a588defdbc43a5d107f596d3
Nelson Glauber <nelson@mokriya.com>
2017-07-06 08:40:56 -0300

    Ajuste no exemplo de mapa para usar o getMapAsync. Sugestão de Thiago Militino

--- Cap15/Mapas/app/src/main/java/dominando/android/mapas/MainActivity.java
+++ Cap15/Mapas/app/src/main/java/dominando/android/mapas/MainActivity.java
@@ -43,6 +43,7 @@ import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -290,8 +291,15 @@ public class MainActivity extends AppCompatActivity implements
         if (mGoogleMap == null) {
             SupportMapFragment fragment = (SupportMapFragment)
                     getSupportFragmentManager().findFragmentByTag(TAG);
-            mGoogleMap = fragment.getMap();
-            mGoogleMap.setOnMapLongClickListener(MainActivity.this);
+            fragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    mGoogleMap = googleMap;
+                    mGoogleMap.setOnMapLongClickListener(MainActivity.this);
+                    atualizarMapa();
+                }
+            });
+            return;
         }
 
         mGoogleMap.clear();
